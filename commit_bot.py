e41fc9eeb87787378970b8a6247e634e7c33332f
RickyBGamez <rickyb5@live.ca>
2025-02-12 18:22:59 -0700

    more organization

--- commit_bot.py
+++ commit_bot.py
@@ -25,17 +25,18 @@ from random import random, randint # Generating a random float between 0 and 1.
 from datetime import datetime, date, timedelta # Date and time for our file.
 
 # Check if a cronjob exists for this script, if not, create it using crontab.
-system("crontab -l > cron.txt")
-with open("cron.txt", "r") as f:
-    if "commit_bot.py" not in f.read():
-        with open("cron.txt", "a") as f:
-            f.write(f"{CRON_JOB_TIME} cd {Path.cwd()} && python3 commit_bot.py\n")
+def check_cronjob():
+    system("crontab -l > cron.txt")
+    with open("cron.txt", "r") as f:
+        if "commit_bot.py" not in f.read():
+            with open("cron.txt", "a") as f:
+                f.write(f"{CRON_JOB_TIME} cd {Path.cwd()} && python3 commit_bot.py\n")
+                f.close()
+                system("crontab cron.txt")
+                system("rm -f cron.txt")
+        else:
             f.close()
-            system("crontab cron.txt")
             system("rm -f cron.txt")
-    else:
-        f.close()
-        system("rm -f cron.txt")
 
 # Logging.
 def log(message):
@@ -78,6 +79,7 @@ if __name__ == "__main__":
     
     if start_date and end_date:
         log(f"Dates provided: {start_date} - {end_date}, running in manual mode.")
+        
         current_date = start_date
         while current_date <= end_date:
             commits = randint(0, MAX_COMMITS)
@@ -85,11 +87,13 @@ if __name__ == "__main__":
                 # create_commit(current_date.strftime("%Y-%m-%d %H:%M:%S"))
                 pass
             log(f"Sucessfully committed {commits} time(s) on {current_date.strftime('%m-%d-%Y')}.")
+            current_date += timedelta(days=1)
         # system("git push")
-        current_date += timedelta(days=1)
+        log(f"Sucessfully pushed all commits to Git.")
     else:
         log(f"No dates were provided. Running in cron mode.")
-        # Execute the script.
+        check_cronjob()
+
         if (random() > NO_COMMIT_CHANCE):
             commits = randint(0, MAX_COMMITS)
             for i in range(commits): # TODO: Re-enable this later.
